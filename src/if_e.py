ede3be0587859e4ccf4f52cc97f3b0ae045cbde9
sys113 <24222077+sys113@users.noreply.github.com>
2020-06-11 14:03:17 +0430

    bug show clutter in windows 7 fixed

--- src/if_e.py
+++ src/if_e.py
@@ -4,6 +4,7 @@ from time import sleep
 from sys import exit
 from pathlib import Path
 from platform import system as os_type
+from platform import release as os_release
 from os.path import isfile
 
 home = (str(Path.home()))
@@ -17,10 +18,16 @@ elif os_type().upper() == 'LINUX':
 else:
   pass
 
-class c:
-  BLUE = '\033[94m'
-  GREEN = '\033[92m'
-  RED = '\033[91m'
+if os_release() == '7':
+	class c:
+		BLUE = ''
+		GREEN = ''
+		RED = ''
+else:
+	class c:
+		BLUE = '\033[94m'
+		GREEN = '\033[92m'
+		RED = '\033[91m'
 
 def if_e():
   if language == 'FA':
